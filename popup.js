b959c33a80d809e8ec4484e987e67d151ac0cb14
King-of-Infinite-Space <27502578+King-of-Infinite-Space@users.noreply.github.com>
2023-03-29 17:33:54 -0400

    + select destination

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ import { postToNotion } from "./sender/notionDb.js"
 const _status = document.getElementById('status')
 const _button = document.getElementById('submitButton')
 const _inputBox = document.getElementById('inputBox')
+const _form = document.getElementById('form')
 
 window.onload = () => {
     getData()
@@ -15,13 +16,15 @@ function prettifyJson(json, indent = 2, level = 2) {
     return s
 }
 
-async function postData(data, source, destination) {
+async function postData(data, source) {
+    const destination = document.getElementById('destinations').value
     const send = {
         notion: postToNotion,
     }
+    _status.innerText = `sending from ${source} to ${destination}...`
     const r = await send[destination](data, source)
 
-    _status.innerText = prettifyJson(r)
+    _status.innerText += `\n${prettifyJson(r)}`
     _status.style.display = 'block'
     if (r.success) {
         _status.classList.add('success')
@@ -35,13 +38,12 @@ async function getData() {
     chrome.tabs.sendMessage(tabs[0].id, { source: "popup" }, function (response) {
         if (response) {
             _inputBox.value = prettifyJson(response.data)
-            _inputBox.style.display = 'block'
+            _form.style.display = 'block'
             _status.innerText = ''
-            _button.style.display = 'block'
             _button.onclick = () => {
                 _status.classList.remove('error')
                 _status.classList.remove('success')
-                postData(response.data, response.source, response.destination)
+                postData(response.data, response.source)
             }
         } else {
             document.getElementById('status').innerText = 'no data'
